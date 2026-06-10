64b140acc517682a4f9238ac2160b993290c3aa5
Muhammad Afzaal Afzal <muafzal@student.42heilbronn.de>
2026-06-10 14:24:12 +0200

    feat: add rotate functions for stacks A and B to position

---
+++ src/rotate_utils.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muafzal <muafzal@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/10 13:18:50 by muafzal           #+#    #+#             */
+/*   Updated: 2026/06/10 14:23:40 by muafzal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	rotate_a_to_pos(t_data *d, int pos)
+{
+	if (pos <= d->a.size / 2)
+	{
+		while (pos-- > 0)
+			ra(d);
+	}
+	else
+	{
+		pos = d->a.size - pos;
+		while (pos-- > 0)
+			rra(d);
+	}
+}
+
+void	rotate_b_to_pos(t_data *d, int pos)
+{
+	if (pos <= d->b.size / 2)
+	{
+		while (pos-- > 0)
+			rb(d);
+	}
+	else
+	{
+		pos = d->b.size - pos;
+		while (pos-- > 0)
+			rrb(d);
+	}
+}
